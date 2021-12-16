14cfcbc06461962a0b0ec412f30f940074723e87
yasserksay <yasser2012507@gmail.com>
2021-12-17 02:58:31 +0300

    final vesion

--- PrinterStore/scripts/script.js
+++ PrinterStore/scripts/script.js
@@ -2,172 +2,137 @@ function formsubmit(){
  var userName= document.getElementById("username1").value;
  var password= document.getElementById("password1").value;
  var errorMessage=document.getElementById("error1");
- var errorMessage1=document.getElementById("error11");
  var errorMessage0=document.getElementById("error0");
- var errorMessage00=document.getElementById("error00");
  var check=true;
- errorMessage.style.display="None";
- if(userName===""){
-     errorMessage0.innerText="Error Fill Username before submit";
+ 
+ //UserName
+ if(userName.length<4){
+    errorMessage0.innerText="Error Fill Username before submit and at least 4 char";
      errorMessage0.style.color="red";
      errorMessage0.style.display="block";
-     errorMessage00.innerText="";
         check=false;
- }else if(userName.length<4){
-    errorMessage00.innerText="Error enter at least 4 char in username";
-     errorMessage00.style.color="red";
-     errorMessage00.style.display="block";
-     errorMessage0.innerText="";
-     check=false;
  }else{
-    errorMessage0.innerText="";
-    errorMessage00.innerText="";
+     errorMessage0.innerText="";
+     errorMessage0.style.display="none";
  }
- if(password===""){
-    errorMessage.innerText="Error Fill password before submit";
+
+ //Password
+ if(password.length<6){
+    errorMessage.innerText="Error Fill Password before submit and at least 6 char";
     errorMessage.style.color="red";
     errorMessage.style.display="block";
-    errorMessage.style.textAlign="center";
-    errorMessage1.innerText="";
-    check= false;
- }else if(password.length<6){
-    errorMessage1.innerText="Error enter at least 6 char in password";
-    errorMessage1.style.color="red";
-    errorMessage1.style.display="block";
-    errorMessage1.style.textAlign="center";
-    errorMessage.innerText="";
-    check= false;
+    check=false;
  }else{
     errorMessage.innerText="";
-    errorMessage1.innerText="";
+    errorMessage.style.display="none";
  }
-
  return check;
-    
-
 }
+
+
 function feedbacksubmitform(){
     var name=document.getElementById("name1").value;
     errorMessage2=document.getElementById("error2");
-    errorMessage22=document.getElementById("error22");
+    // errorMessage22=document.getElementById("error22");
     errorMessage222=document.getElementById("error222");
 
     var check=true;
     
     // condition for name
-    if(name===""){
-        errorMessage2.innerText="Error Fill name before submit";
+    if(name.length<6){
+        errorMessage2.innerText="Please fill name and make sure at least 6 char";
         errorMessage2.style.color="red";
         errorMessage2.style.display="none";
-        errorMessage22.innerText="";
         check=false;
-     }
-    else if(name.length<6){
-        errorMessage22.innerText="Error enter at least 6 char in name";
-        errorMessage22.style.color="red";
-        errorMessage22.style.display="none";
+    }else{
         errorMessage2.innerText="";
-        check=false;
-    } 
-    else{
-        errorMessage2.innerText="";
-        errorMessage22.innerText="";
-
+        
     }
     if(checknum()===-1){
+        console.log(checknum());
         check=false;
     }else{
         errorMessage222.innerText="";
+        
     }
     
     // condition for phone number
     var phone=document.getElementById("phone1").value;
-    var checkphone=phone.search(/^9665/);
-    var errorMessage33= document.getElementById("error33");
+    //var checkphone=phone.search(/^9665\d{8}/);
+    // var errorMessage33= document.getElementById("error33");
     var errorMessage3= document.getElementById("error3");
 
-    if(checkphone===-1){
-        errorMessage33.innerText="enter a number begin with (9665)";
-        errorMessage33.style.color="red";
-        errorMessage33.style.display="none";
-        check=false;
-    }else{
-        errorMessage33.innerText="";
-    }
-    if(phone.length!==12){
-        errorMessage3.innerText="Enter a number that contain only 12 digits";
+    if(phone.search(/^(9665[\d]{8})$|^$/)!=0){
+        errorMessage3.innerText="enter a number begin with (9665) and 8 Digit Or Empty";
         errorMessage3.style.color="red";
         errorMessage3.style.display="none";
         check=false;
     }else{
         errorMessage3.innerText="";
+        
+        //errorMessage33.style.display="none";
     }
+    
 
     
     // condition for Email Address
     var email=document.getElementById("email1").value;
     errorMessage4=document.getElementById("error4")
-    errorMessage44=document.getElementById("error44")
-
-    if(email===""){
-        errorMessage44.innerText="Please Fill the Email before submit";
-        errorMessage44.style.color="red";
-        errorMessage44.style.display="none";
-        check=false;
-    }else{
-        errorMessage44.innerText="";
-
-    }
-    if(email.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)===-1) {
-        errorMessage4.innerText="Email not in formal way";
+    
+    if(email.search(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)!=0) {
+        errorMessage4.innerText="Please Fill Email in formal way And Not Empty";
         errorMessage4.style.color="red";
         errorMessage4.style.display="none";
         check=false;
     }else{
         errorMessage4.innerText="";
+        
     }
 
     // condition for subject
     var subject=document.getElementById("subject1").value;
     errorMessage5=document.getElementById("error5");
-    if(subject===""){
+    if(subject==""){
         errorMessage5.innerText="Please Enter Subject before submit";
         errorMessage5.style.color="red";
         errorMessage5.style.display="none";
         check=false;
     }else{
         errorMessage5.innerText="";
+        
     }
     
     //condition for topics
     var topics=document.getElementById("topics1").value;
     errorMessage6=document.getElementById("error6");
-    if(topics==="Select a Topics"){
+    if(topics=="Select a Topics"){
         errorMessage6.innerText="Please select topics from the list";
         errorMessage6.style.color="red";
         errorMessage6.style.display="none";
         check=false;
     }else{
         errorMessage6.innerText="";
+        
     }
 
-    //condition for topics
+    //condition for Describtion
     var describtion=document.getElementById("describtion1").value;
     errorMessage7=document.getElementById("error7");
-    if(describtion===""){
+    if(describtion==""){
         errorMessage7.innerText="Please enter some info in describtion";
         errorMessage7.style.color="red";
         errorMessage7.style.display="none";
         check=false;
     }else{
         errorMessage7.innerText="";
+        
     }
+    
      finalerror=document.getElementById("finalerror");
      
      finalerror.innerText=errorMessage2.innerText+"\n"
-     +errorMessage22.innerText+"\n"+errorMessage222.innerText+"\n"
-     +errorMessage3.innerText+"\n"+errorMessage33.innerText
-     +"\n"+errorMessage4.innerText+"\n"+errorMessage44.innerText+"\n"
+     +errorMessage222.innerText+"\n"
+     +errorMessage3.innerText+"\n"+errorMessage4.innerText+"\n"
      +errorMessage5.innerText+"\n"+errorMessage6.innerText+"\n"
      +errorMessage7.innerText;
 
